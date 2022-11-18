729ef33f7b2f5ea8aa0ec558f9bfd538cd4e5458
Patricia Misur <patricia.misur@ericsson.com>
2022-11-18 17:45:09 +0100

    Firs refactoring steps

--- matrix_inverse.py
+++ matrix_inverse.py
@@ -1,26 +1,36 @@
-import numpy as np
+# import numpy as np
+from typing import List
+import sys
 
 class Matrix:
     rows: int
     cols: int
-    elements: list
+    elements: List[float]
 
-    def __init__(self, rows:int, cols:int, elements:list):
-        self.rows:int = rows
-        self.cols:int = cols
-        self.elements:list = elements
+    def __init__(self, rows: int, cols: int, elements: List[float]):
+        self.rows = rows
+        self.cols = cols
+        self.elements = elements
 
     @property
     def determinant(self) -> float:
         if self.rows != self.cols:
             raise ValueError("Number of rows and coloumns must be equal!")
+
+        represented_matrix = []
+        for i in range(0, len(list), self.rows):
+            represented_matrix.append(self.elements[i:i + self.rows])
+
+        represented_matrix = [self.elements[i:i + self.rows]
+                              for i in range(0, len(list), self.rows)]
+
         def func(list, n):
             for i in range(0, len(list), n):
                 yield list[i:i + n]
         represented_matrix = (list(func(self.elements, self.rows)))
         determinant = np.linalg.det(represented_matrix)
         return determinant
-   
+
     def ___repr___(self):
         print("\n")
         for i in range(self.rows):
@@ -28,31 +38,46 @@ class Matrix:
                 print(self.elements[self.cols * i + j], end='   ')
             print("\n")
 
-def read_matrix_from_file(file, rows, cols) -> Matrix:   #should be able to read given part of the file
-    matrix = Matrix(rows=rows, cols=cols, elements=[])
-    with open(file) as f:
-        for element in next(f).split(','):
-            matrix.elements.append(int(element))
-        for line in f:
+    def add(m: 'Matrix') -> 'Matrix':
+        pass
+
+    @staticmethod
+    def read_from_file(file, rows, cols) -> 'Matrix':   #should be able to read given part of the file
+        matrix = Matrix(rows=rows, cols=cols, elements=[])
+        with open(file) as f:
+            line = next(f)
             for element in line.split(','):
                 matrix.elements.append(int(element))
-    return matrix
+
+            line = next(f)
+            for element in line.split(','):
+                matrix.elements.append(int(element))
 
 
-def indentity_matrix_generator(rows, cols) -> Matrix:
-    if rows != cols:
-        raise ValueError("Number of rows and coloumns must be equal!")
-    matrix = Matrix(rows=rows, cols=cols, elements=[])
-    for i in range(rows):
-        for j in range(cols):
-            if i == j:
-                matrix.elements.append(1)
-            else:
-                matrix.elements.append(0)
-    return matrix
+            for line in f:
+                for element in line.split(','):
+                    matrix.elements.append(int(element))
+        return matrix
+
+    @staticmethod
+    def indentity(rows, cols) -> 'Matrix':
+        if rows != cols:
+            raise ValueError("Number of rows and coloumns must be equal!")
+        matrix = Matrix(rows=rows, cols=cols, elements=[])
+        for i in range(rows):
+            for j in range(cols):
+                if i == j:
+                    matrix.elements.append(1)
+                else:
+                    matrix.elements.append(0)
+        return matrix
 
 
+def main(argv):
+    # print(indentity_matrix_generator(4,4).___repr___())
+    print(Matrix.read_from_file('matrix.txt', 3, 3).___repr___())
+    # print(indentity_matrix_generator(3,3).determinant)
 
-print(indentity_matrix_generator(4,4).___repr___())
-print(read_matrix_from_file('matrix.txt', 3, 3).___repr___())
-print(indentity_matrix_generator(3,3).determinant)
+
+if __name__ == '__main__':
+    main(sys.argv)
